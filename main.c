2db21624cf34fcace1b973d7cb483c50bdcca723
Stephan Doan <sdoan@stuy.edu>
2016-11-21 11:42:12 -0500

    start forking

--- main.c
+++ main.c
@@ -3,19 +3,47 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 
 #include "shell.h"
 
+void sigHandle(int sigNum){
+  if(sigNum ==SIGINT){
+    printf("SIGINT needs me, I'm out\n");
+    exit(0);
+  }
+  if(sigNum == SIGUSR1){
+    printf("My parent PID: %d\n",getppid());
+  }
+}
+
 int main(){
+  signal(SIGINT, sigHandle);
+  signal(SIGUSR1, sigHandle);
   char *comm;
   char *s[256];
   comm = (char*)malloc(sizeof(char *));
-  fgets(comm, 255, stdin);
   int i = 0;
   char *temp;
-  temp = strstr(comm,"\n");
-  int index;
-  index = temp - comm;
-  comm[index] = 0;
+  int index, f;
+  while(1){
+    printf("waddya want:");
+    fgets(comm, 255, stdin);
+    temp = strstr(comm,"\n");
+    index = temp - comm;
+    comm[index] = 0;
+    f = fork();
+    if(f){
+      while (comm){
+	s[i]=strsep(&comm, " ");
+	i++;
+      }
+      s[i] = NULL;
+      if(execvp(s[0], s)){
+	printf("sumting wong in child\n");
+	printf("%s\n", strerror(errno));
+      }
+    }
+  }
   return 0;
 }
