4323fd09f4018d3c53d07ad333b9c4a8f0ce7633
Matthew Zhu <mzhu3@stuy.edu>
2016-11-28 11:41:10 -0500

    redirecting problems

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "shell.h"
 
@@ -16,7 +19,53 @@ void sigHandle(int sigNum){
     printf("My parent PID: %d\n",getppid());
   }
 }
+int * cd(char *dir){
+}
+int check(char * comm){
+  const char *shellComms = "cd,>,>>,<,<<,|,&";
+  int def = -1;
+  char *temp;
+  while(comm){
+    temp = strsep(shellComms,",");
+    if(!strcmp(comm,temp)){
+      def = 0;
+    }
+  }
+  return def;
+}
+int * pipe(char *comm){
+}
+int *redirWrite(char *comm){
+  char * commSplit;
+  char * file2;
+  int i =0;
+  int checker = 1;
+  while(i < strlen(comm)){
+    if(strcmp(comm[i]," ")){
+      i++;
+    }
+    else{
+      checker = 0;
+    }
+  }
+  if(checker){
+    int i = 2;
+    for(;i>0;i--){
+      file2 = strsep(comm," ");
+    }
+  }
+  else{
+    commSplit = strstr(comm,">");
+    strncpy(file2,commSplit + 1;strlen(commSplit)-1);
+  }
+}
 
+  commSplit = strstr(comm,">");
+  char * fileName;
+  strcpy(commSplit,fileName,
+}
+int * redirApp(char *comm){
+}
 int main(){
   signal(SIGINT, sigHandle);
   signal(SIGUSR1, sigHandle);
