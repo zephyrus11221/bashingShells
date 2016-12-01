6b2c00034bdf3bcfea5840d6205919a593b2fc84
Matthew Zhu <mzhu3@stuy.edu>
2016-12-01 11:41:58 -0500

    fixed check

--- shell.h
+++ shell.h
@@ -1,8 +1,8 @@
 #ifndef SHELL_COMMANDS_h
 #define SHELL_COMMANDS_h
-int * cd(char *dir);//changes cwd, dir is target. Returns 0 for success, -1 for error.
+int * cd1(char *dir);//changes cwd, dir is target. Returns 0 for success, -1 for error.
 int * check(char *comm);//checks if comm is one of our functions, returns 0 for yes, -1 for no
-int * pipe(char *comm);//pipes
+int * pipe1(char *comm);//pipes
 int * redirWrite(char *comm);//calls functions using given redirection, overwrite
 int * redirApp(char *comm);//calls functions using given redirection, append
 
