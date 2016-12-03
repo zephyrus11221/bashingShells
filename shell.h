79e1c2ddf2130512b2ddf0af5ae060cf71626380
mzhu3 <mzhu3@stuy.edu>
2016-12-02 23:52:56 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_COMMANDS_h
 #define SHELL_COMMANDS_h
+void sigHand(int sigNum);//creates handle for user signal inputs
 void commCentral(char *comm);//checks for shell programs, and prepares the child to run them. Includes pipe, cd, and redirections
 void rmChar (char arr[], int start);//removes the char at the index equal to start of the array myarray[]
 #endif
